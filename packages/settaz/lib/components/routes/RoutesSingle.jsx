93bca8cd474fc20d5fa2e726d5d0f59a1c4f2e75
Jeff Lau <jeff-lau@live.com>
2020-03-09 01:21:20 +0800

    Add todo list

--- packages/settaz/lib/components/routes/RoutesSingle.jsx
+++ packages/settaz/lib/components/routes/RoutesSingle.jsx
@@ -1,39 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import styled from '@emotion/styled'
-import fetch from 'node-fetch'
 import InstagramEmbed from 'react-instagram-embed'
 
 const RoutesSingleContainer = styled('div')`
-  background: green;
   margin-bottom: 20px;
 `
 
 export default function RoutesSingle({ route }) {
   const { name, grade, description, videos } = route
-  const [embed, setEmbed] = useState(null)
-  //instagr.am/p/fA9uwTtkSN/
 
-  useEffect(() => {
-    fetch('https://api.instagram.com/oembed?hidecaption=true&url=' + videos[0])
-      .then(res => {
-        console.log('res', res)
-        return res.json()
-      })
-      .then(json => {
-        console.log('json', json)
-        setEmbed(json.html)
-      })
-  }, [])
-
-  // useEffect(() => {
-  //   fetch(videos[0] + 'media')
-  //     .then(res => {
-  //       console.log(res)
-  //       setUrl(res.url)
-  //       return res.json()
-  //     })
-  //     .then(console.log)
-  // }, [])
   return (
     <RoutesSingleContainer>
       {name} - {grade}
